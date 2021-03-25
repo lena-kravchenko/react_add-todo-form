c040e5ecc522d859a52b133bb749f0973429d103
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-25 16:16:30 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.daae17e6ce9e0c792ebbf183d085bdf6.js"
 );
 
 self.addEventListener('message', (event) => {
